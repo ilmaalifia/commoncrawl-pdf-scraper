07430cb8348c7686c8afa6b182435b412d857c24
ilmaalifia <ilma.mahardika@gmail.com>
2025-04-07 15:52:42 +0200

    feat: Apply asyncio

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -2,59 +2,45 @@ import argparse
 import asyncio
 import json
 import re
-import time
 from datetime import datetime
 from multiprocessing import cpu_count
-from queue import Queue
 from random import random
-from typing import Literal
 from urllib.parse import urljoin
 
 import aiohttp
 import requests
 from bs4 import BeautifulSoup
 from milvus import Milvus
-from tenacity import (
-    AsyncRetrying,
-    retry,
-    retry_if_exception_type,
-    stop_after_attempt,
-    wait_exponential,
-)
+from tenacity import AsyncRetrying, retry, stop_after_attempt, wait_exponential
 from utils import USER_AGENT, setup_logger
 from vectorisation import Vectorisation
 
 logger = setup_logger()
 INDEX_SERVER = "http://index.commoncrawl.org"
 URLS = [
+    # "https://liftoff.energy.gov/vpp/",
     "*.gov",
-    "*.com",
-    "*.org",
-    "*.edu",
-    "*.net",
-    "*.co",
+    # "*.com",
+    # "*.org",
+    # "*.edu",
+    # "*.net",
+    # "*.co",
 ]
 MIME_TYPES = [
     "pdf",
     "html",
 ]
-jobs_queue = Queue()
+pagination_url_queue = asyncio.Queue()
+absolute_url_queue = asyncio.Queue()
 vect = Vectorisation()
 mv = Milvus()
 counter = {"success": 0, "failed": 0, "empty_or_duplicate": 0}
 failed = {}
 
 
-def is_retryable_error(e):
-    return isinstance(
-        e, (aiohttp.ClientError, asyncio.TimeoutError, ConnectionError, TimeoutError)
-    ) or ("Failed to open stream" in str(e))
-
-
 @retry(
     stop=stop_after_attempt(3),
     wait=wait_exponential(multiplier=1, min=1, max=10),
-    retry=retry_if_exception_type(is_retryable_error),
 )
 def get_index_api(index_name):
     session = requests.Session()
@@ -63,59 +49,116 @@ def get_index_api(index_name):
     return collinfo[0]["cdx-api"] if collinfo else None
 
 
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_exponential(multiplier=1, min=1, max=10),
+)
 def get_num_pages(index_api, url):
     params = {
         "url": url,
         "showNumPages": True,
     }
-
-    session = requests.Session()
-    r = session.get(index_api, params=params).json()
-    pages = r.get("pages")
-    return pages if pages else None
-
-
-@retry(
-    stop=stop_after_attempt(3),
-    wait=wait_exponential(multiplier=1, min=1, max=10),
-    retry=retry_if_exception_type(is_retryable_error),
-)
-def get_jobs_from_url(
-    index_api, url, page, format: Literal["pdf", "html"] = "pdf", limit=None
-):
-    """
-    Fetch absolute urls from the Common Crawl index API and add them to the jobs queue.
-    """
-
-    jobs = []
-    params = {
-        "url": url,
-        "filter": [f"~mime:.*/{format}$", "=status:200"],
-        "output": "json",
-        "fl": "url,timestamp,mime",
-        "page": page,
-    }
-
-    if limit:
-        params["limit"] = limit
-
+    pages = 1
     try:
-        response = requests.get(
-            index_api,
-            params=params,
-            headers={"user-agent": USER_AGENT, "accept": "application/json"},
-        )
-        if response.status_code == 200:
-            jobs = response.content.decode("utf-8").splitlines()
-            for job in jobs:
-                jobs_queue.put(json.loads(job))
-        else:
-            logger.warning(
-                f"Failed to fetch jobs from {response.url}. Status code: {response.status_code}."
-            )
-        time.sleep(random() * 2)
+        session = requests.Session()
+        r = session.get(index_api, params=params).json()
+        pages = r.get("pages", 1)
     except requests.exceptions.RequestException as e:
-        logger.error(f"Failed to fetch {index_api}: {e}")
+        logger.error(f"Failed to fetch num pages {index_api}: {e}")
+    finally:
+        return pages
+
+
+async def fetch_pagination_url(session, job):
+    """
+    Fetch list of absolute urls from the Common Crawl pagination index API and add them to the jobs queue.
+    """
+    jobs = []
+    index_api = job.pop("index_api", None)
+
+    logger.info(f"Fetching pagination url for {job['url']} page {job['page']}")
+    try:
+        async for attempt in AsyncRetrying(
+            stop=stop_after_attempt(3),
+            wait=wait_exponential(multiplier=1, min=1, max=10),
+        ):
+            with attempt:
+                async with session.get(
+                    index_api,
+                    params=job,
+                    headers={"user-agent": USER_AGENT, "accept": "application/json"},
+                    timeout=10,
+                ) as response:
+                    jobs = await response.read()
+                    if jobs:
+                        jobs = jobs.splitlines()
+                        for job in jobs:
+                            await absolute_url_queue.put(json.loads(job))
+    except aiohttp.ClientResponseError as e:
+        logger.warning(
+            f"Failed to fetch pagination url {e.request_info.url}: {e.status} {e.message}"
+        )
+    finally:
+        await asyncio.sleep(random() * 2)
+
+
+async def fetch_absolute_url_pdf(session, job):
+    try:
+        async for attempt in AsyncRetrying(
+            stop=stop_after_attempt(3),
+            wait=wait_exponential(multiplier=1, min=1, max=10),
+        ):
+            with attempt:
+                async with session.get(
+                    job["url"], timeout=10, headers={"user-agent": USER_AGENT}
+                ) as response:
+                    response.raise_for_status()
+                    pdf_bytes = await response.read()
+                    is_duplicate = await mv.is_duplicate(job["url"], len(pdf_bytes))
+                    if is_duplicate:
+                        counter["empty_or_duplicate"] += 1
+                    else:
+                        vector_data = await vect.generate_embeddings_from_pdf_bytes(
+                            pdf_bytes, job["url"], job["timestamp"]
+                        )
+                        inserted = await mv.insert_data(vector_data)
+
+                        if inserted.get("insert_count", 0) > 0:
+                            counter["success"] += 1
+                            logger.info(
+                                f"Successfully inserted {inserted['insert_count']} data of {job['url']}"
+                            )
+                        else:
+                            counter["empty_or_duplicate"] += 1
+    except Exception as e:
+        counter["failed"] += 1
+        failed[job["url"]] = str(e)
+        logger.error(f"Failed to fetch pdf {job['url']}: {e}")
+    finally:
+        logger.info(
+            f"Success: {counter['success']}, Failed: {counter['failed']}, Empty or Duplicate: {counter['empty_or_duplicate']}"
+        )
+
+
+async def fetch_absolute_url_html(session, job):
+    try:
+        async for attempt in AsyncRetrying(
+            stop=stop_after_attempt(3),
+            wait=wait_exponential(multiplier=1, min=1, max=10),
+        ):
+            with attempt:
+                async with session.get(
+                    job["url"], timeout=10, headers={"user-agent": USER_AGENT}
+                ) as response:
+                    response.raise_for_status()
+                    html = await response.text()
+                    pdf_urls = await find_pdf_url_from_html(html, job["url"])
+                    for pdf_url in pdf_urls:
+                        await fetch_absolute_url_pdf(
+                            session, {"url": pdf_url, "timestamp": job["timestamp"]}
+                        )
+    except Exception as e:
+        logger.error(f"Failed to fetch html {job['url']}: {e}")
 
 
 async def find_pdf_url_from_html(html_content, base_url):
@@ -142,112 +185,94 @@ async def find_pdf_url_from_html(html_content, base_url):
     return pdf_urls
 
 
-async def fetch_job_pdf(session, job):
-    try:
-        async for attempt in AsyncRetrying(
-            stop=stop_after_attempt(3),
-            wait=wait_exponential(multiplier=1, min=1, max=10),
-            retry=retry_if_exception_type(is_retryable_error),
-        ):
-            with attempt:
-                async with session.get(
-                    job["url"], timeout=10, headers={"user-agent": USER_AGENT}
-                ) as response:
-                    pdf_bytes = await response.read()
-                    is_duplicate = await mv.is_duplicate(job["url"], len(pdf_bytes))
-                    if is_duplicate:
-                        counter["empty_or_duplicate"] += 1
-                    else:
-                        vector_data = await vect.generate_embeddings_from_pdf_bytes(
-                            pdf_bytes, job["url"], job["timestamp"]
-                        )
-                        inserted = await mv.insert_data(vector_data)
-
-                        if inserted.get("insert_count", 0) > 0:
-                            counter["success"] += 1
-                            logger.info(
-                                f"Successfully inserted {inserted['insert_count']} data of {job['url']}"
-                            )
-                        else:
-                            counter["empty_or_duplicate"] += 1
-    except Exception as e:
-        counter["failed"] += 1
-        failed[job["url"]] = str(e)
-        logger.error(f"Failed to fetch pdf {job['url']}: {e}")
+async def pagination_producer(index_api):
+    logger.info(f"Starting pagination producer for {index_api}")
+    for url in URLS:
+        num_pages = get_num_pages(index_api, url)
+        if num_pages:
+            for page in range(num_pages):
+                for mime_type in MIME_TYPES:
+                    await pagination_url_queue.put(
+                        {
+                            "index_api": index_api,
+                            "url": url,
+                            "filter": [f"~mime:.*/{mime_type}$", "=status:200"],
+                            "output": "json",
+                            "fl": "url,timestamp,mime",
+                            "page": page,
+                        }
+                    )
+    logger.info(
+        f"Finished pagination producer for {index_api}. Pagination queue size: {pagination_url_queue.qsize()}"
+    )
 
 
-async def fetch_job_html(session, job):
-    try:
-        async for attempt in AsyncRetrying(
-            stop=stop_after_attempt(3),
-            wait=wait_exponential(multiplier=1, min=1, max=10),
-            retry=retry_if_exception_type(is_retryable_error),
-        ):
-            with attempt:
-                async with session.get(
-                    job["url"], timeout=10, headers={"user-agent": USER_AGENT}
-                ) as response:
-                    html = await response.text()
-                    pdf_urls = await find_pdf_url_from_html(html, job["url"])
-                    for pdf_url in pdf_urls:
-                        await fetch_job_pdf(
-                            session, {"url": pdf_url, "timestamp": job["timestamp"]}
-                        )
-    except Exception as e:
-        logger.error(f"Failed to fetch html {job['url']}: {e}")
+async def pagination_url_consumer(session):
+    logger.info(f"Starting pagination url consumer")
+    while True:
+        try:
+            job = await pagination_url_queue.get()
+            if job:
+                await fetch_pagination_url(session, job)
+                pagination_url_queue.task_done()
+        except asyncio.CancelledError:
+            break
+    logger.info(
+        f"Finished pagination url consumer. Absolute url queue size: {absolute_url_queue.qsize()}"
+    )
 
 
-async def worker(session):
-    while not jobs_queue.empty():
-        job = jobs_queue.get()
-        if "pdf" in job["mime"]:
-            await fetch_job_pdf(session, job)
-        elif "html" in job["mime"]:
-            await fetch_job_html(session, job)
-        jobs_queue.task_done()
+async def absolute_url_consumer(session):
+    logger.info(f"Starting absolute url consumer")
+    while True:
+        try:
+            job = await absolute_url_queue.get()
+            if job:
+                logger.info(f"Processing absolute url: {job.get('url')}")
+                if "pdf" in job.get("mime", ""):
+                    await fetch_absolute_url_pdf(session, job)
+                elif "html" in job.get("mime", ""):
+                    await fetch_absolute_url_html(session, job)
+                absolute_url_queue.task_done()
+        except asyncio.CancelledError:
+            break
+
+    logger.info(f"Finished absolute url consumer")
 
 
-async def run_workers(num_workers):
+async def run_workers(num_workers, index_api):
     async with aiohttp.ClientSession() as session:
-        tasks = [worker(session) for _ in range(num_workers)]
-        await asyncio.gather(*tasks)
+        await pagination_producer(index_api)
+
+        pagination_url_consumers = [
+            asyncio.create_task(pagination_url_consumer(session))
+            for _ in range(num_workers)
+        ]
+
+        absolute_url_consumers = [
+            asyncio.create_task(absolute_url_consumer(session))
+            for _ in range(num_workers)
+        ]
+
+        await pagination_url_queue.join()
+        await absolute_url_queue.join()
+
+        for c in pagination_url_consumers + absolute_url_consumers:
+            c.cancel()
+
+        await asyncio.gather(*pagination_url_consumers, *absolute_url_consumers)
 
 
-def test():
-    jobs = [
-        {
-            "timestamp": "20241204155509",
-            "url": "https://liftoff.energy.gov/vpp/",
-            "mime": "text/html",
-        },
-        {
-            "timestamp": "20241213055142",
-            "url": "https://repository.library.noaa.gov/pdfjs/web/viewer.html?file=https://repository.library.noaa.gov/view/noaa/48516/noaa_48516_DS1.pdf",
-            "mime": "text/html",
-        },
-        {
-            "timestamp": "20250324122124",
-            "url": "https://www.36thdistrictcourtmi.gov/docs/default-source/general-information/ncsc-reports/admin_ncsc-d36-final-report_-20140612.pdf?sfvrsn=2",
-            "mime": "application/pdf",
-        },
-    ]
-
-    for job in jobs:
-        jobs_queue.put(job)
-
+def generate_num_workers():
     try:
         num_workers = cpu_count() * 2
     except NotImplementedError:
         num_workers = 4
-
-    asyncio.run(run_workers(num_workers=num_workers))
-    mv.reindex()
-    logger.info(counter)
-    logger.info(json.dumps(failed))
+    return num_workers
 
 
 def main():
-    parser = argparse.ArgumentParser(description="CommonCrawl PDF Scraper")
+    parser = argparse.ArgumentParser(description="PDF Scraper")
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
         "--index-name",
@@ -268,30 +293,16 @@ def main():
     index_name = get_index_api(raw_index_name)
 
     if index_name:
-        logger.info(f"Processing index {index_name}")
-        for url in URLS:
-            num_pages = get_num_pages(index_name, url)
-            if num_pages:
-                for page in range(num_pages):
-                    logger.info(f"Processing page {page}/{num_pages - 1} for {url}")
-                    for mime_type in MIME_TYPES:
-                        get_jobs_from_url(
-                            index_name, url, page, format=mime_type, limit=10
-                        )
-        try:
-            num_workers = cpu_count() * 2
-        except NotImplementedError:
-            num_workers = 4
-
-        asyncio.run(run_workers(num_workers=num_workers))
+        num_workers = generate_num_workers()
+        asyncio.run(run_workers(num_workers, index_name))
         mv.reindex()
-        logger.info(counter)
-        logger.info(json.dumps(failed))
+        logger.info(
+            f"Success: {counter['success']}, Failed: {counter['failed']}, Empty or Duplicate: {counter['empty_or_duplicate']}"
+        )
+        logger.info(f"Failed URLs: {json.dumps(failed)}")
     else:
         logger.warning(f"Index {raw_index_name} not found.")
 
 
 if __name__ == "__main__":
-    # test()
-
     main()
